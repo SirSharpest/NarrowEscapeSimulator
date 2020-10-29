8da6b29a1eb6319da4f466cc2a4cb3c670c59b39
Nathan <nathan1hughes@gmail.com>
2020-10-29 17:47:38 +0000

    Updated examples to illustrate distributed points

--- PyEscape/escape_plan.py
+++ PyEscape/escape_plan.py
@@ -23,7 +23,8 @@ def travel(delta, pa):
 
 
 def escape_with_path(r, delta, dt, max_steps,
-                     pore_locs, pore_size, check_func, cur_pos):
+                     pore_locs, pore_size, check_func,
+                     cur_pos, contain_r):
     """Provides the full path of a particle as it escapes from a container
 
     Takes a radius of container, delta step size, dt difference of time, a
@@ -45,9 +46,11 @@ def escape_with_path(r, delta, dt, max_steps,
         new_pos_steps = 0
         new_pos = travel(delta, cur_pos)
         steps = steps + 1
-        while (not (check_func(new_pos, r)) and new_pos_steps < MAX_NEW_MOVEMENTS):
+        while (not (check_func(new_pos, r)) and
+               new_pos_steps < MAX_NEW_MOVEMENTS):
             for idx, (pd_loc, psize) in enumerate(zip(pore_locs, pore_size)):
-                if passthrough_pore(new_pos, pd_loc, r=psize):
+                if passthrough_pore(new_pos, pd_loc,
+                                    r=psize, contain_r=contain_r):
                     path[steps] = new_pos
                     return (path[:steps], idx)
             new_pos = travel(delta, cur_pos)
@@ -60,7 +63,7 @@ def escape_with_path(r, delta, dt, max_steps,
 def escape(D, vol, pore_size, pore_locs,
            dt=None, seed=None, shape='sphere',
            max_steps=(int(1e7)), with_path=False,
-           random_start=False):
+           random_start=False, contain_r=None):
     """Wrapper function that can be called by a user - used to optimise code
     shared between escape methods
 
@@ -90,15 +93,15 @@ def escape(D, vol, pore_size, pore_locs,
     if with_path:
         return escape_with_path(r, delta, dt,
                                 max_steps, pore_locs,
-                                pore_size, check_func, cur_pos)
+                                pore_size, check_func, cur_pos, contain_r)
 
     return escape_quick(r, delta, dt,
                         max_steps, pore_locs,
-                        pore_size, check_func, cur_pos)
+                        pore_size, check_func, cur_pos, contain_r)
 
 
 def escape_quick(r, delta, dt, max_steps,
-                 pore_locs, pore_size, check_func, cur_pos):
+                 pore_locs, pore_size, check_func, cur_pos, contain_r):
     """Simulates escape without tracking position through container
 
     Takes a radius of container, delta step size, dt difference of time, a
@@ -113,9 +116,11 @@ def escape_quick(r, delta, dt, max_steps,
     while steps < max_steps:
         new_pos = travel(delta, cur_pos)
         new_pos_steps = 0
-        while (not (check_func(new_pos, r)) and new_pos_steps < MAX_NEW_MOVEMENTS):
+        while (not (check_func(new_pos, r)) and
+               new_pos_steps < MAX_NEW_MOVEMENTS):
             for pd_loc in pore_locs:
-                if passthrough_pore(new_pos, pd_loc, r=pore_size):
+                if passthrough_pore(new_pos, pd_loc,
+                                    r=pore_size, contain_r=contain_r):
                     return (steps+1)*dt
             new_pos = travel(delta, cur_pos)
             new_pos_steps += 1
